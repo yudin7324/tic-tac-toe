f59a38c428e6758500a78c41c84c734468584a01
Ilya Yudin <yudin2402@gmail.com>
2025-03-30 19:40:58 +0200

    stylized buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { FC } from 'react'
 
 const App:FC = () => {
   return (
-    <>
-      app
-    </>
+    <div className='main'>
+      <div className='container'>
+
+        <a href="#" className='btn'>BUTTON 1</a>
+        <a href="#" className='btn-2'>BUTTON 1</a>
+        <button className='btn btn--secondary'>BUTTON 1</button>
+        <button className='btn-2 btn-2--secondary'>BUTTON 1</button>
+      </div>
+    </div>
   )
 }
 
