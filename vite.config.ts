0adfefc5afd51be8bbbd6198d90435318218a471
Ilya Yudin <yudin2402@gmail.com>
2025-03-30 16:38:54 +0200

    initial application setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+});
